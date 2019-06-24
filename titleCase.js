f57181d1312587ddc97871887afa132c60c4fcf4
Phil Shin <49408402+PBShin@users.noreply.github.com>
2019-06-23 22:37:24 -0500

    Create titleCase.js

---
+++ titleCase.js
@@ -0,0 +1,27 @@
+// Function that converts strings to "Title Case"
+
+function titleCase(str) {
+  let arr = str.split(" ");
+  let newArr=[];
+  console.log(arr);
+  for (let word of arr) {
+    console.log(word);
+    let firstLetter = word[0];
+    firstLetter = firstLetter.toUpperCase();
+    console.log(firstLetter);
+    let restArr=[];
+    for (let i=1; i<word.length; i++) {
+      let letter = word[i];
+      letter = letter.toLowerCase();
+      restArr.push(letter);
+    }
+    let rest = restArr.join("");
+    //let rest=word.slice(1,word.length);
+    word = firstLetter+rest;
+    newArr.push(word);
+    console.log(newArr);
+  }
+  str = newArr.join(" ");
+  console.log(str);
+  return str;
+}
